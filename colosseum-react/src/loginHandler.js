6ae3d1388f8578e51ad865b917128932f2cf80cf
CastleLong911 <castlelong911@gmail.com>
2023-12-14 02:43:55 +0900

    23/12/14 02:43

--- colosseum-react/src/loginHandler.js
+++ colosseum-react/src/loginHandler.js
@@ -12,7 +12,7 @@ const LoginHandeler = (props) => {
       
       await axios({
         method: "get",
-        url: `http://debatearena:8000/api/login/kakao?code=${code}`,
+        url: `http://debatearena.me:8000/api/login/kakao?code=${code}`,
         headers: {
           //"Content-Type": "application/json;charset=utf-8", //json형태로 데이터를 보내겠다는뜻
           //"Access-Control-Allow-Origin": "*", //이건 cors 에러때문에 넣어둔것. 당신의 프로젝트에 맞게 지워도됨
