86b1802799d48a8d88978a08e938bfb5da16ff35
CastleLong911 <castlelong911@gmail.com>
2023-12-14 11:59:52 +0900

    23/12/14 11:59

--- colosseum-react/src/topicRoom.js
+++ colosseum-react/src/topicRoom.js
@@ -187,7 +187,7 @@ const TopicRoom = (props) => {
         }
 
         const newChatSocket = new WebSocket(
-            'ws://127.0.0.1/ws/chat/' + topicId + '/'
+            'ws://debatearena.me/ws/chat/' + topicId + '/'
         );
 
         newChatSocket.onmessage = (e) => {
