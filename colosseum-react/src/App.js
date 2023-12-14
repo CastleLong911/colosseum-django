3b1fcfe417faa38ecff17853fb78051d8f129087
CastleLong911 <castlelong911@gmail.com>
2023-12-15 07:46:38 +0900

    23/12/15 07:46

--- colosseum-react/src/App.js
+++ colosseum-react/src/App.js
@@ -107,7 +107,7 @@ function Main(){
   }
 
   const fetchRooms = async () => {
-    console.log('version 0.9.1');
+    console.log('version 0.9.2');
     try {
       const response = await fetch(process.env.REACT_APP_DEFAULT_URL+"/api/getRoomInfoAll");
       if (!response.ok) {
--- colosseum/asgi.py
+++ colosseum/asgi.py
@@ -8,17 +8,15 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from channels.security.websocket import AllowedHostsOriginValidator
+
 from django.core.asgi import get_asgi_application
-from api.routing import websocket_urlpatterns
-import api.routing
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'colosseum.settings')
-
 django_asgi_app = get_asgi_application()
 
+from channels.routing import ProtocolTypeRouter, URLRouter
+from api.routing import websocket_urlpatterns
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
     "websocket": URLRouter(
