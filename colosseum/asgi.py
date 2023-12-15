49a4e8189c84505e0abf068a1f217bad82260dc7
CastleLong911 <castlelong911@gmail.com>
2023-12-16 01:36:21 +0900

    23/12/16 01:36

--- colosseum/asgi.py
+++ colosseum/asgi.py
@@ -7,12 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
-import os
+import os, django
 
 from django.core.asgi import get_asgi_application
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'colosseum.settings')
+django.setup()
 django_asgi_app = get_asgi_application()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
